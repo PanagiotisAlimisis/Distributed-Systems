55eaa9836dd6fd53c62bb335943d00ef41a78f6e
panagiotis <panagiotis@home.home>
2022-01-23 23:13:57 +0200

    disable cors and session

--- Backend/src/main/java/gr/hua/ds/freetransportation/security/config/WebSecurityConfig.java
+++ Backend/src/main/java/gr/hua/ds/freetransportation/security/config/WebSecurityConfig.java
@@ -10,9 +10,16 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @EnableWebSecurity
 @Configuration
@@ -46,26 +53,48 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         auth.authenticationProvider(authenticationProvider());
     }
 
-@Override
+
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
+
+//    Disable session
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.cors();
+
         http
-            .csrf().disable()
-            .authorizeRequests()
-            .antMatchers("/admin/**").hasAnyAuthority(RoleTypes.ADMIN.toString())
-            .antMatchers("/api/unemployment_application/**").hasAnyAuthority(RoleTypes.DEFAULT_USER.toString())
-            .antMatchers("/api/oaed_employee/**").hasAnyAuthority(RoleTypes.OAED_EMPLOYEE.toString())
-            .antMatchers("/api/unemployed/**").hasAnyAuthority(RoleTypes.UNEMPLOYED.toString())
-            .antMatchers("/api/transportation_employee/**").hasAnyAuthority(RoleTypes.TRANSPORTATION_EMPLOYEE.toString())
-            .antMatchers("/api/register/**").permitAll()
-            .antMatchers("/api/login/**").permitAll()
-            .anyRequest().authenticated()
-            .and().formLogin().loginPage("/login")
-            .usernameParameter("email").permitAll()
-            .defaultSuccessUrl("/admin/home", true)
-            .and().exceptionHandling().accessDeniedPage("/admin/forbidden")
-            .and()
-            .logout().logoutUrl("/logout")
-            .and().httpBasic();
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/admin/**").hasAnyAuthority(RoleTypes.ADMIN.toString())
+                .antMatchers("/api/unemployment_application/**").hasAnyAuthority(RoleTypes.DEFAULT_USER.toString())
+                .antMatchers("/api/oaed_employee/**").hasAnyAuthority(RoleTypes.OAED_EMPLOYEE.toString())
+                .antMatchers("/api/unemployed/**").hasAnyAuthority(RoleTypes.UNEMPLOYED.toString())
+                .antMatchers("/api/transportation_employee/**").hasAnyAuthority(RoleTypes.TRANSPORTATION_EMPLOYEE.toString())
+                .antMatchers("/api/register/**").permitAll()
+                .antMatchers("/api/login/**").permitAll()
+                .anyRequest().authenticated()
+                .and().formLogin().loginPage("/login")
+                .usernameParameter("email").permitAll()
+                .defaultSuccessUrl("/admin/home", true)
+                .and().exceptionHandling().accessDeniedPage("/admin/forbidden")
+                .and()
+                .logout().logoutUrl("/logout")
+                .and().httpBasic();
+    }
+
+
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        // Don't do this in production, use a proper list  of allowed origins
+        config.setAllowedOriginPatterns(Collections.singletonList("*"));
+        config.setAllowedHeaders(Arrays.asList("Origin", "Content-Type", "Accept"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "OPTIONS", "DELETE", "PATCH"));
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 
 }
