f9c0c7d54ac633930738f9c8c6221eb443c6fd4a
palimisis <panagiotisalimisis@gmail.com>
2022-06-21 13:29:29 +0300

    added jenkins and ansible

--- src/main/java/gr/hua/ds/freetransportation/admin/controllers/LoginController.java
+++ src/main/java/gr/hua/ds/freetransportation/admin/controllers/LoginController.java
@@ -4,10 +4,36 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+//@Getter
+//@Setter
+//@Configuration
+//@ConfigurationProperties(prefix = "spring.mail")
+//@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class LoginController {
 
+//    @Autowired
+//    private final JavaMailSender mailSender;
+
+//    private String username;
+
+
     @GetMapping("/login")
-    public String viewLoginPage() {
+    public String viewLoginPage() throws Exception {
+//        this.sendEmail();
         return "login";
     }
+
+//    public void sendEmail() throws Exception {
+//        MimeMessage message = mailSender.createMimeMessage();
+//
+//        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(message, true);
+//
+//        mimeMessageHelper.setTo("it21902@hua.gr");
+//        mimeMessageHelper.setFrom("it21902@hua.gr");
+//        mimeMessageHelper.setSubject("FREE TRANSPORTATION SYSTEM");
+//        mimeMessageHelper.setText("You just logged in.");
+//
+//        mailSender.send(message);
+//    }
+
 }
